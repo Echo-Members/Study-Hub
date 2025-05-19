6501e4b05eb3477ada04d645ef1217a1251307f4
Echo-Members <Membersatecho@gmail.com>
2025-05-20 00:17:11 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,15 +1,16 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+document.getElementById("contactForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    
+
     let email = document.getElementById("email").value;
     let subject = document.getElementById("subject").value;
     let description = document.getElementById("description").value;
     let phone = document.getElementById("phone").value;
-    
+
     if (email === "" || subject === "" || description === "" || phone === "") {
         alert("Please fill out all required fields.");
     } else {
-        document.getElementById("responseMessage").textContent = "Your message has been submitted. Our team will contact you soon!";
+        document.getElementById("responseMessage").textContent =
+            "✅ Your message has been submitted. Our team will contact you soon!";
         document.getElementById("contactForm").reset();
     }
 });
