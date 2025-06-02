9881082660584220c400596eccb231434213c326
Echo-Members <Membersatecho@gmail.com>
2025-06-03 01:49:01 +0530

    Add files via upload

---
+++ STUDY HUB/JAVASCRIPT/Courses.js
@@ -0,0 +1,7 @@
+        function toggleSidebar() {
+            document.getElementById('sidebar').classList.toggle('active');
+        }
+
+        function toggleSubMenu(element) {
+            element.classList.toggle('open');
+        }
